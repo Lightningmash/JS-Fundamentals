eea020a4e63d0c016e1c656c39742833c36d5422
Lightningmash <144124299+Lightningmash@users.noreply.github.com>
2025-05-26 21:04:12 +0300

    Create 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,15 @@
+const sizeArg = process.argv[2];
+const size = parseInt(sizeArg);
+
+if (isNaN(size) || size <= 0) {
+    console.log('Missing size');
+} else {
+    const char = 'X';
+    for (let i = 0; i < size; i++) {
+        let row = '';
+        for (let j = 0; j < size; j++) {
+            row += char;
+        }
+        console.log(row);
+    }
+}
